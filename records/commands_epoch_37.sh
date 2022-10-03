a4d2e5f86d01551571195b77ace3d27552340411
lampardlb <lampardthu@gmail.com>
2022-10-03 14:11:12 +0800

    epoch 37 payout records

---
+++ records/commands_epoch_37.sh
@@ -0,0 +1,200 @@
+export $(cat ~/.mina-env | xargs)
+
+mina accounts import -privkey-path ~/cb_keys/my-wallet
+
+export SUPERCHARGED_POOL=B62qmvHQzJmT2rKE1F9RemenGRG8BfXT1Kurve3eT4iC2HMrWiaVG3H
+
+mina accounts unlock -public-key $SUPERCHARGED_POOL 
+
+mina client send-payment -amount 2032.854626473 -receiver B62qrUfmK7TrJwNXPiAk5FL3Q3GB8zaYQBY8CqUBoSy2s7b4VCHNZHV -fee 0.001 -sender $SUPERCHARGED_POOL -memo Supercharged_pool -nonce 1536
+sleep 1s 
+echo current payment with nonce 1536 done.
+mina client send-payment -amount 1653.972639952 -receiver B62qjK9SbkfNdY5dfhaZkT51EDEUSqiVUQbrQSEbEWSFFyGdNzCNBap -fee 0.001 -sender $SUPERCHARGED_POOL -memo Supercharged_pool -nonce 1537
+sleep 1s 
+echo current payment with nonce 1537 done.
+mina client send-payment -amount 597.852383139 -receiver B62qnv7Ls2whuQ15F3XucBR8sLFznXiKFtLd7uyyzsi5BMFBkVrFzoT -fee 0.001 -sender $SUPERCHARGED_POOL -memo Supercharged_pool -nonce 1538
+sleep 1s 
+echo current payment with nonce 1538 done.
+mina client send-payment -amount 565.877228630 -receiver B62qifCH3cJnUGcvw2aZiLXFLqLHDyRLAvZU79vZYJb7LaGetWH3Fwi -fee 0.001 -sender $SUPERCHARGED_POOL -memo Supercharged_pool -nonce 1539
+sleep 1s 
+echo current payment with nonce 1539 done.
+mina client send-payment -amount 467.773106898 -receiver B62qq5Evw7ZhPvCdn4v87hGWunLd9fWAZgvtSywsuJ2hfVFnPCXjoAb -fee 0.001 -sender $SUPERCHARGED_POOL -memo Supercharged_pool -nonce 1540
+sleep 1s 
+echo current payment with nonce 1540 done.
+mina client send-payment -amount 210.182898794 -receiver B62qitxwTUR7MhvEFp14bg7f6j3wMMGvjivesQKzLazAwU6MCdsUKMW -fee 0.001 -sender $SUPERCHARGED_POOL -memo Supercharged_pool -nonce 1541
+sleep 1s 
+echo current payment with nonce 1541 done.
+mina client send-payment -amount 206.679073239 -receiver B62qpG5NXUYN5SE3ZaFwPftACTpLXnT4K2XYoQJDDUaWWuMGeWirwuQ -fee 0.001 -sender $SUPERCHARGED_POOL -memo Supercharged_pool -nonce 1542
+sleep 1s 
+echo current payment with nonce 1542 done.
+mina client send-payment -amount 198.864232163 -receiver B62qoVnUa8UnwZgLDXaMPYAHfzwWWWyTTt6VViAdcMpJqcRr3L1ZCN9 -fee 0.001 -sender $SUPERCHARGED_POOL -memo Supercharged_pool -nonce 1543
+sleep 1s 
+echo current payment with nonce 1543 done.
+mina client send-payment -amount 198.004592639 -receiver B62qosjams8KHovBonwFGYDdMG3LeJA2am6HE2gTpFGLoJXao6GT1s7 -fee 0.001 -sender $SUPERCHARGED_POOL -memo Supercharged_pool -nonce 1544
+sleep 1s 
+echo current payment with nonce 1544 done.
+mina client send-payment -amount 165.537591000 -receiver B62qkQtCcAvirZbJCPZwQYFsPURjKPi18pB6fu6r7MbKxbK5GCs5xxm -fee 0.001 -sender $SUPERCHARGED_POOL -memo Supercharged_pool -nonce 1545
+sleep 1s 
+echo current payment with nonce 1545 done.
+mina client send-payment -amount 154.580051309 -receiver B62qpZyzDyQG1HUw7KNsksTVzbXTa8JyG3cBJQaESgJzSri5Qe4VHWe -fee 0.001 -sender $SUPERCHARGED_POOL -memo Supercharged_pool -nonce 1546
+sleep 1s 
+echo current payment with nonce 1546 done.
+mina client send-payment -amount 154.216218367 -receiver B62qmJeVUiEkcr9CcfGmuEzqELAmeoSRpN9KZrbrzXdb9WCGZGjY7Dz -fee 0.001 -sender $SUPERCHARGED_POOL -memo Supercharged_pool -nonce 1547
+sleep 1s 
+echo current payment with nonce 1547 done.
+mina client send-payment -amount 152.391882356 -receiver B62qmGBqPjYLTtaTGjnKPGHNBNh6FFMs35yoxRUaTZunAqC2xX8HtSS -fee 0.001 -sender $SUPERCHARGED_POOL -memo Supercharged_pool -nonce 1548
+sleep 1s 
+echo current payment with nonce 1548 done.
+mina client send-payment -amount 135.016861268 -receiver B62qm9QMVXdyWrcG1QgW8pQTb7BpHx8Kv1sXU9vMbGAntoyCfXwJmNW -fee 0.001 -sender $SUPERCHARGED_POOL -memo Supercharged_pool -nonce 1549
+sleep 1s 
+echo current payment with nonce 1549 done.
+mina client send-payment -amount 124.652398709 -receiver B62qo91xNJxdFK177rwdTW7QrzfwnhDJHo7JkqSqKU5xzJWgD1ZiKXT -fee 0.001 -sender $SUPERCHARGED_POOL -memo Supercharged_pool -nonce 1550
+sleep 1s 
+echo current payment with nonce 1550 done.
+mina client send-payment -amount 121.575272999 -receiver B62qnQCdt1QSBcJ4K4WWcR1Mm6f15ih779LR9juCu5yAUn4hZ17v5eT -fee 0.001 -sender $SUPERCHARGED_POOL -memo Supercharged_pool -nonce 1551
+sleep 1s 
+echo current payment with nonce 1551 done.
+mina client send-payment -amount 108.550550478 -receiver B62qku7ZGL9RG2rfQ395F4ouk9eux2iUA4SnEUP5iAJnqTHiMDzKzPD -fee 0.001 -sender $SUPERCHARGED_POOL -memo Supercharged_pool -nonce 1552
+sleep 1s 
+echo current payment with nonce 1552 done.
+mina client send-payment -amount 99.876058570 -receiver B62qpkS3v1UXYYUVttsn6GzHzHm3PtgjnVV1ZsTAF5KwZPLxmG1Lma2 -fee 0.001 -sender $SUPERCHARGED_POOL -memo Supercharged_pool -nonce 1553
+sleep 1s 
+echo current payment with nonce 1553 done.
+mina client send-payment -amount 85.072264693 -receiver B62qkN13FRberGWgfhU5QkppAvJxpQtq26oqLwKuQ4DAikTNWmkt6SJ -fee 0.001 -sender $SUPERCHARGED_POOL -memo Supercharged_pool -nonce 1554
+sleep 1s 
+echo current payment with nonce 1554 done.
+mina client send-payment -amount 80.060265961 -receiver B62qjnTp4kNLtMhDyyJWCotQdEgwPdkLXFTauuFAUYYBiod7Hj9TPJL -fee 0.001 -sender $SUPERCHARGED_POOL -memo Supercharged_pool -nonce 1555
+sleep 1s 
+echo current payment with nonce 1555 done.
+mina client send-payment -amount 65.446973995 -receiver B62qmMkbajiY3bdVRjv5bx3yxjp5sBHHQuu8M11sDdjtBy5VdgWdgTr -fee 0.001 -sender $SUPERCHARGED_POOL -memo Supercharged_pool -nonce 1556
+sleep 1s 
+echo current payment with nonce 1556 done.
+mina client send-payment -amount 62.323593802 -receiver B62qjdDCyxrFhnAzR32qgViJtna88ysoSsTbYowVX6iDdA8SGsWShnB -fee 0.001 -sender $SUPERCHARGED_POOL -memo Supercharged_pool -nonce 1557
+sleep 1s 
+echo current payment with nonce 1557 done.
+mina client send-payment -amount 51.402141740 -receiver B62qn45bEJnU2BSmu13xfkfcZmYEn8D6rWAsdzQS6SaWGWqEJDhm6q2 -fee 0.001 -sender $SUPERCHARGED_POOL -memo Supercharged_pool -nonce 1558
+sleep 1s 
+echo current payment with nonce 1558 done.
+mina client send-payment -amount 40.012399731 -receiver B62qjsiS7ynezJPc3FnzPe9isRh53xqftuQ7H2TRK3vanGFr76m7Uyx -fee 0.001 -sender $SUPERCHARGED_POOL -memo Supercharged_pool -nonce 1559
+sleep 1s 
+echo current payment with nonce 1559 done.
+mina client send-payment -amount 30.738048936 -receiver B62qkGVCPGHrLHV9r3G9NqD9du2Wcb5uKBfDvLwbxDKnyjMppHoN6gi -fee 0.001 -sender $SUPERCHARGED_POOL -memo Supercharged_pool -nonce 1560
+sleep 1s 
+echo current payment with nonce 1560 done.
+mina client send-payment -amount 29.764480226 -receiver B62qpUpEH7ivz61Sxo3PuTxntUNswmiHo2jwE2HvqmSVYW7q8xoStkx -fee 0.001 -sender $SUPERCHARGED_POOL -memo Supercharged_pool -nonce 1561
+sleep 1s 
+echo current payment with nonce 1561 done.
+mina client send-payment -amount 27.057828287 -receiver B62qm4HceSuB9Pds5TgEipa4bhThvezuoDozJmY4SfeNodD4WgqXNge -fee 0.001 -sender $SUPERCHARGED_POOL -memo Supercharged_pool -nonce 1562
+sleep 1s 
+echo current payment with nonce 1562 done.
+mina client send-payment -amount 22.229693363 -receiver B62qmu46DVv9f86QkJtvdP5dNbzeCeYNy91jqDM4ysDttfrqn3xnGpp -fee 0.001 -sender $SUPERCHARGED_POOL -memo Supercharged_pool -nonce 1563
+sleep 1s 
+echo current payment with nonce 1563 done.
+mina client send-payment -amount 19.500632932 -receiver B62qrMFp53j42qU9VSwpX67JCnpiGkT689K5xZa6Nk1C2sfwZzfAfMB -fee 0.001 -sender $SUPERCHARGED_POOL -memo Supercharged_pool -nonce 1564
+sleep 1s 
+echo current payment with nonce 1564 done.
+mina client send-payment -amount 18.245102557 -receiver B62qnHGcWskvaooRUJ6RpJrSPuGb62zv52ytPYVpixaQWMM9hRm1MZp -fee 0.001 -sender $SUPERCHARGED_POOL -memo Supercharged_pool -nonce 1565
+sleep 1s 
+echo current payment with nonce 1565 done.
+mina client send-payment -amount 17.240218912 -receiver B62qmnzi1YHFYxLaK1tDQzhVd3FAsbcGiG3UXqXGoDr7s8NDpS3DYtk -fee 0.001 -sender $SUPERCHARGED_POOL -memo Supercharged_pool -nonce 1566
+sleep 1s 
+echo current payment with nonce 1566 done.
+mina client send-payment -amount 15.044845069 -receiver B62qkBMtTuTguHAmjABKGfX2C4ca6WZuTj2yXFB7B5VsB7aSxFz7gnG -fee 0.001 -sender $SUPERCHARGED_POOL -memo Supercharged_pool -nonce 1567
+sleep 1s 
+echo current payment with nonce 1567 done.
+mina client send-payment -amount 13.795800959 -receiver B62qjZnptvfm9q911jSZHZDQSb1GfaLwwZGsPvbERdcb42BRNZ5P31x -fee 0.001 -sender $SUPERCHARGED_POOL -memo Supercharged_pool -nonce 1568
+sleep 1s 
+echo current payment with nonce 1568 done.
+mina client send-payment -amount 13.530185992 -receiver B62qkA4dujnHueHA4yHN7P6VzRYwujYRYeHpLmquQcM8BNeGZQkVD5R -fee 0.001 -sender $SUPERCHARGED_POOL -memo Supercharged_pool -nonce 1569
+sleep 1s 
+echo current payment with nonce 1569 done.
+mina client send-payment -amount 11.357220681 -receiver B62qo9ey7aGSt87ZkvTj9yM51zWorjP3M6qU54i6YccyucmA5eVM6wT -fee 0.001 -sender $SUPERCHARGED_POOL -memo Supercharged_pool -nonce 1570
+sleep 1s 
+echo current payment with nonce 1570 done.
+mina client send-payment -amount 11.323897873 -receiver B62qnYVJSEPGrVtUvECksn4cRDBa8c4QhLQ7dzoHxqB6kPcAZvY41ui -fee 0.001 -sender $SUPERCHARGED_POOL -memo Supercharged_pool -nonce 1571
+sleep 1s 
+echo current payment with nonce 1571 done.
+mina client send-payment -amount 8.795601386 -receiver B62qn4BeEJCM31mH2Wg8fA4ecdtwoPjotXPJMRB8WGToPPixtjA7LgE -fee 0.001 -sender $SUPERCHARGED_POOL -memo Supercharged_pool -nonce 1572
+sleep 1s 
+echo current payment with nonce 1572 done.
+mina client send-payment -amount 7.337512371 -receiver B62qjBukZi32eQ1MCrfVAswWesWzXyLbUWJbpTwkk7BDzwU7zPMP87b -fee 0.001 -sender $SUPERCHARGED_POOL -memo Supercharged_pool -nonce 1573
+sleep 1s 
+echo current payment with nonce 1573 done.
+mina client send-payment -amount 5.558803125 -receiver B62qqHccT9Hh4vNLvieMaKV6fXMJPE7tFaymeu17VrJhxVQ8Y6aBWR6 -fee 0.001 -sender $SUPERCHARGED_POOL -memo Supercharged_pool -nonce 1574
+sleep 1s 
+echo current payment with nonce 1574 done.
+mina client send-payment -amount 5.015745682 -receiver B62qjprMJnzvJqrdYCMm5wnnam8QqBZWASsL1T7KX1uzEhWhZjnBWxM -fee 0.001 -sender $SUPERCHARGED_POOL -memo Supercharged_pool -nonce 1575
+sleep 1s 
+echo current payment with nonce 1575 done.
+mina client send-payment -amount 4.510593795 -receiver B62qkHAZshmphiL13WYgXN55FFPfrQA7jGWPaobVwvgYmq7eZnaL62e -fee 0.001 -sender $SUPERCHARGED_POOL -memo Supercharged_pool -nonce 1576
+sleep 1s 
+echo current payment with nonce 1576 done.
+mina client send-payment -amount 3.938440001 -receiver B62qkSAN6FX99nguNJNQW3fdXh5yEv5PtMZwW6KWyUZrSRdTyHpdC6r -fee 0.001 -sender $SUPERCHARGED_POOL -memo Supercharged_pool -nonce 1577
+sleep 1s 
+echo current payment with nonce 1577 done.
+mina client send-payment -amount 3.474223870 -receiver B62qnVhFK2aSGDyrazemQgyDtmPd36wo2g8TTmyeHJWLKTpLtoL8H5x -fee 0.001 -sender $SUPERCHARGED_POOL -memo Supercharged_pool -nonce 1578
+sleep 1s 
+echo current payment with nonce 1578 done.
+mina client send-payment -amount 3.458320407 -receiver B62qmkmSCiTvayykE34nVjvyGdYDvFhx1iHV94rHgkESSNiApTzixj9 -fee 0.001 -sender $SUPERCHARGED_POOL -memo Supercharged_pool -nonce 1579
+sleep 1s 
+echo current payment with nonce 1579 done.
+mina client send-payment -amount 3.096628611 -receiver B62qjkMjDoYHs4fKMm6Lnv3rcFpQaoBtjxnHhsqdCsqFYRSZb3SnFDG -fee 0.001 -sender $SUPERCHARGED_POOL -memo Supercharged_pool -nonce 1580
+sleep 1s 
+echo current payment with nonce 1580 done.
+mina client send-payment -amount 1.893689068 -receiver B62qiefFsu75RVCL8cBjz1iq8d1p5X1wbX8iddmEUsq9UwuDrimPuo5 -fee 0.001 -sender $SUPERCHARGED_POOL -memo Supercharged_pool -nonce 1581
+sleep 1s 
+echo current payment with nonce 1581 done.
+mina client send-payment -amount 1.462032195 -receiver B62qkpL1kdnYVKAQ5oTSN5QG2oz1xAJ9TwvS7VKCWu9D3oSc931dKP6 -fee 0.001 -sender $SUPERCHARGED_POOL -memo Supercharged_pool -nonce 1582
+sleep 1s 
+echo current payment with nonce 1582 done.
+mina client send-payment -amount 0.727082137 -receiver B62qqjbLrvRf3afa22MC6fkoYvPJVeEFY5QbRs8uRySn9pZ7U6cWW48 -fee 0.001 -sender $SUPERCHARGED_POOL -memo Supercharged_pool -nonce 1583
+sleep 1s 
+echo current payment with nonce 1583 done.
+mina client send-payment -amount 0.707776065 -receiver B62qiwkRmsD8T2zbkSjvadmbJ5EpuaGebvD3pMDQ5gqsRj5bgTjdmCM -fee 0.001 -sender $SUPERCHARGED_POOL -memo Supercharged_pool -nonce 1584
+sleep 1s 
+echo current payment with nonce 1584 done.
+mina client send-payment -amount 0.619014566 -receiver B62qp5779JznHu89EpPxwmB1DLuY5aucG385GGXhodzQ18KJNLJG5cY -fee 0.001 -sender $SUPERCHARGED_POOL -memo Supercharged_pool -nonce 1585
+sleep 1s 
+echo current payment with nonce 1585 done.
+mina client send-payment -amount 0.469259137 -receiver B62qoHQLxNcPyNWPeqU1M23YZY2SLPaTr8m4Dg14Z65NMY6v3c8yZMJ -fee 0.001 -sender $SUPERCHARGED_POOL -memo Supercharged_pool -nonce 1586
+sleep 1s 
+echo current payment with nonce 1586 done.
+mina client send-payment -amount 0.243970089 -receiver B62qiUCGYVUykUk5e2uK2hvrDx4CQ6Z1DQRxE4DFNXArE7KYD1ngq8f -fee 0.001 -sender $SUPERCHARGED_POOL -memo Supercharged_pool -nonce 1587
+sleep 1s 
+echo current payment with nonce 1587 done.
+mina client send-payment -amount 0.204730755 -receiver B62qiYihpWA6auBZun2ayZZhnf5bsT1qyq5LvDCk1piokgykeWM7j5M -fee 0.001 -sender $SUPERCHARGED_POOL -memo Supercharged_pool -nonce 1588
+sleep 1s 
+echo current payment with nonce 1588 done.
+mina client send-payment -amount 0.196792568 -receiver B62qp52atR13HaUJWJHCcg57tKjK1gwqeBnxL2RS7E53DeBWpnmLYUN -fee 0.001 -sender $SUPERCHARGED_POOL -memo Supercharged_pool -nonce 1589
+sleep 1s 
+echo current payment with nonce 1589 done.
+mina client send-payment -amount 0.146574619 -receiver B62qowhKT5TfrZuAjXdfXMhHhCTWtVegiC1YRLiF8gQN3aLEjiSSjAg -fee 0.001 -sender $SUPERCHARGED_POOL -memo Supercharged_pool -nonce 1590
+sleep 1s 
+echo current payment with nonce 1590 done.
+mina client send-payment -amount 0.122654833 -receiver B62qpn3hY83x3acuJ4XTFqZmbYfAim76AXjfduxbrn1uEJv8YGafNz2 -fee 0.001 -sender $SUPERCHARGED_POOL -memo Supercharged_pool -nonce 1591
+sleep 1s 
+echo current payment with nonce 1591 done.
+mina client send-payment -amount 0.112396730 -receiver B62qjt5GzHKrNZNx5gNBn9ebrAhHRQ62BmwvS5BvcLRUb2R1zqch28s -fee 0.001 -sender $SUPERCHARGED_POOL -memo Supercharged_pool -nonce 1592
+sleep 1s 
+echo current payment with nonce 1592 done.
+mina client send-payment -amount 0.103287850 -receiver B62qrCEGNwJFLjBkZRbQpbYzwMCjtjsqRipJWqhse3SchFbrFGJXiZp -fee 0.001 -sender $SUPERCHARGED_POOL -memo Supercharged_pool -nonce 1593
+sleep 1s 
+echo current payment with nonce 1593 done.
+mina client send-payment -amount 0.069473823 -receiver B62qiab7PzuV56zkmiBoyN7rX3rxtPebDd65aTzPnwYUfyf2aWFKUy8 -fee 0.001 -sender $SUPERCHARGED_POOL -memo Supercharged_pool -nonce 1594
+sleep 1s 
+echo current payment with nonce 1594 done.
+mina client send-payment -amount 0.066152939 -receiver B62qpjieFM2nJdeTpnkvcN5FUuzwNxsMkWAGsW9eYdTXdGvfMrkHC9d -fee 0.001 -sender $SUPERCHARGED_POOL -memo Supercharged_pool -nonce 1595
+sleep 1s 
+echo current payment with nonce 1595 done.
+mina client send-payment -amount 0.049609506 -receiver B62qkLdEr6XTWAcFxrdMcHfrQmMeLwZ1AJi7piw6sJvLV33vu2uK3V7 -fee 0.001 -sender $SUPERCHARGED_POOL -memo Supercharged_pool -nonce 1596
+sleep 1s 
+echo current payment with nonce 1596 done.
+mina client send-payment -amount 0.036417770 -receiver B62qrDQCSgiGv4dWwfSsGxPu2ny6AJd8FARFJyJmu14RmxhBmDZoimo -fee 0.001 -sender $SUPERCHARGED_POOL -memo Supercharged_pool -nonce 1597
+sleep 1s 
+echo current payment with nonce 1597 done.
+mina client send-payment -amount 0.033521247 -receiver B62qkKAt2A8G3Q2v1dnNBPmMdmi9fWPA6yTrNzPtqqLLQzGuF5usxv9 -fee 0.001 -sender $SUPERCHARGED_POOL -memo Supercharged_pool -nonce 1598
+sleep 1s 
+echo current payment with nonce 1598 done.
+mina client send-payment -amount 0.019321260 -receiver B62qieZHH1wHfv4PkSBhyttENUSV34ywjBP1LoedSiaz6sePMqy6ik3 -fee 0.001 -sender $SUPERCHARGED_POOL -memo Supercharged_pool -nonce 1599
+sleep 1s 
+echo current payment with nonce 1599 done.
